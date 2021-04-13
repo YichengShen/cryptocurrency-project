cdecbe4f0b9307e713893c5499aab0d43f5df5db
Neo Zhou <zhouaea@bc.edu>
2021-04-13 17:03:40 -0400

    added tcp functionality

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"./initialization"
+	"./tcp"
 	"fmt"
 	"./telog"
 )
 
 func main() {
+	// Read information from command line and configuration file to obtain the node the process should act as
+	// and a list of valid nodes to send messages to.
+	node, nodes := initialization.InitializeNode()
+
+	// Set up TCP listening for process, continuously listening for messages and printing them out.
+	go node.UnicastReceive()
+
+	// Continuously scan user input for instructions on which node to send a message to.
+	tcp.ExecuteCommands(node, nodes)
+
 	log := telog.Telog{}
 	log.Init()
 
--- transactions/transactions.go
+++ transactions/transactions.go
@@ -1,1 +1,10 @@
 package transactions
+
+// Transfer coins from one public key to another.
+// TODO: Add coin ids.
+type Transaction struct {
+	signature string
+	sender string
+	recipient string
+	coins int
+}
