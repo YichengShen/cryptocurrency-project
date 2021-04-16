c2f115532831a6054e47c2e4ad2a53e21e8d8955
YichengShen <jsszsyc@hotmail.com>
2021-04-15 23:16:00 -0400

    add e-signature and transaction

--- main.go
+++ main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"./telog"
+	"./node"
+	"./tx"
 )
 
 func main() {
-	log := telog.Telog{}
-	log.Init()
+	//log := telog.Telog{}
+	//log.Init()
+	//
+	//log.AddBlock("Goofy mints 5 dollars")
+	//log.AddBlock("Goofy paid Alice 5 dollars")
+	//log.AddBlock("Alice paid Bob 5 dollars")
+	//
+	//fmt.Println("# Blocks in Log:", log.GetNumBlocks())
+	//
+	//fmt.Println("Log is valid:", log.Check())
+	//
+	//log.Attack(1)
+	//
+	//fmt.Println("After attack, Log is valid:", log.Check())
 
-	log.AddBlock("Goofy mints 5 dollars")
-	log.AddBlock("Goofy paid Alice 5 dollars")
-	log.AddBlock("Alice paid Bob 5 dollars")
+	transactions := tx.TxArray{}
 
-	fmt.Println("# Blocks in Log:", log.GetNumBlocks())
+	nodes := node.Nodes{}
+	alice := nodes.NewNode("Alice")
+	bob := nodes.NewNode("Bob")
 
-	fmt.Println("Log is valid:", log.Check())
+	transactions.AppendNewTx("CoinMaker", "Bob", &tx.CoinMakerPk, &bob.Pk, &tx.CoinMakerSk, 200)
+	transactions.AppendNewTx("CoinMaker", "Alice", &tx.CoinMakerPk, &alice.Pk, &tx.CoinMakerSk, 100)
+	transactions.AppendNewTx("CoinMaker", "Bob", &tx.CoinMakerPk, &bob.Pk, &tx.CoinMakerSk, 200)
+	transactions.AppendNewTx("Alice", "Bob", &alice.Pk, &bob.Pk, &alice.Sk, 50)
+	transactions.AppendNewTx("Bob", "Alice", &bob.Pk, &alice.Pk, &bob.Sk, 150)
 
-	log.Attack(1)
+	transactions.PrintTxHistory()
 
-	fmt.Println("After attack, Log is valid:", log.Check())
 }
 
 
